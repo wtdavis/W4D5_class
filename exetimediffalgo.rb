9d09247e33a47f6ea0b0eb84e4cbe7d5ac0b750b
wtdavis <wtdavis@wesleyan.edu>
2023-02-03 14:00:36 -0500

    exec time differences done

--- exetimediffalgo.rb
+++ exetimediffalgo.rb
@@ -17,3 +17,39 @@ end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 p my_min_2(list)  # =>  -5
+
+def largest_contiguous_subsum_1(arr)
+  subarrs = []
+  (0...arr.length).each do |idx1|
+    (idx1...arr.length).each do |idx2|
+      subarrs << arr[idx1..idx2]
+    end
+  end
+  return subarrs.map {|subarr| subarr.sum}.max
+end
+list = [5, 3, -7]
+p largest_contiguous_subsum_1(list)
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum_1(list) # => 8 (from [7, -6, 7])
+list = [-5, -1, -3]
+p largest_contiguous_subsum_1(list) 
+
+
+def largest_contiguous_subsum_2(arr)
+    current_sum = 0
+    largest_sum = nil
+    arr.each do |ele|
+      current_sum += ele
+      largest_sum = current_sum if largest_sum == nil || current_sum > largest_sum 
+      current_sum = 0 if current_sum < 0 
+    end
+    return largest_sum
+end
+
+list = [5, 3, -7]
+p largest_contiguous_subsum_2(list)
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum_2(list) # => 8 (from [7, -6, 7])
+list = [-5, -1, -3]
+p largest_contiguous_subsum_2(list) 
+
